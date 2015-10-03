f4f7bd53f144dfea2f4ba0301f855bd02347649d
AR <alir6716@gmail.com>
2015-10-03 18:57:05 +0100

    More changes to 404.js

--- js/404.js
+++ js/404.js
@@ -10,23 +10,31 @@ switch (window.location.pathname) {
 	// Moved URLs
 	case "/confetti":
 		goTo(base + "/fun/confetti")
+		break;
 	case "/name":
 		goTo(base + "/fun/name")
+		break;
 	case "/particle":
 		goTo(base + "/fun/particle")
+		break;
 	case "/ribbon":
 		goTo(base + "/fun/ribbon")
+		break;
 	case "/trail":
 		goTo(base + "/fun/trail")
+		break;
 	
 	// Others
 	case "/314":
 		goTo(base + "/personal/txts/pi_nospace.txt")
+		break;
 	case "/src":
 	case "/source":
 		goTo("https://github.com/alir6716/alir6716.github.io")
+		break;
 	case "/insta":
 		goTo(base + "/personal/insta")
+		break;
 	
 	// Default
 	default:
