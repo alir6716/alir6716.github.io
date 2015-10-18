55a4b7c0bc60181d239797dc56e1d27a77dc5aee
AR <alir6716@gmail.com>
2015-10-18 13:01:39 +0100

    Blank lines removed. (scripts/urlparams.js

--- scripts/urlparams.js
+++ scripts/urlparams.js
@@ -1,3 +1,2 @@
 // urlparams.js, Created by AR.
 function getURLParameter(e){return decodeURIComponent((new RegExp("[?|&]"+e+"=([^&;]+?)(&|#|;|$)").exec(location.search)||[,""])[1].replace(/\+/g,"%20"))||void 0}function getHashParameter(e){return decodeURIComponent((new RegExp("[#|&]"+e+"=([^&;]+?)(&|#|;|$)").exec(location.hash)||[,""])[1].replace(/\+/g,"%20"))||void 0}
-
