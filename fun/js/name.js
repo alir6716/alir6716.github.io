fa0130febca7fc86db7319efa87e6766245ee6b5
AR <alir6716@gmail.com>
2015-10-18 13:08:38 +0100

    Bug fixed. (fun/js/name.js)

--- fun/js/name.js
+++ fun/js/name.js
@@ -1,3 +1,8 @@
+var name = getURLParameter("name")
+if (name === undefined) {
+	name = "Ali Raja"
+}
+
 var white = [0, 0, 100];
 var black = [0, 0, 27];
 var red = [0, 100, 63];
@@ -6,7 +11,7 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = getURLParameter('name');
+var myName = name;
 var letterColors = [red, orange, green, blue, purple];
 
 var bubbleShape = "circle"
