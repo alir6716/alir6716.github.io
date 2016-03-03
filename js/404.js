fd9ed6f93c65653ea0abd97d5e6b76a1d8939e14
AR <alir6716@gmail.com>
2016-03-03 19:44:32 +0000

    /js/404.js updated.

--- js/404.js
+++ js/404.js
@@ -6,62 +6,65 @@
  */
 
 function goTo(dest) {
-	document.getElementById("page-title").innerHTML = "Redirecting... | AR"
+	document.getElementById("page-title").innerHTML = "Redirecting... | AR";
 	// location.hash now supported.
 	window.location = dest + window.location.search + window.location.hash;
 }
 
-var base = "http://alir6716.github.io"
+var base = "http://alir6716.github.io";
 
 switch (window.location.pathname) {
 	/* Moved URLs */
+	case "/insta":
+		goTo(base + "/insta/");
+		break;
 	case "/insta/beta":
 	case "/insta/beta/":
-		goTo(base + "/insta")
+		goTo(base + "/insta");
 		break;
 	case "/personal/insta":
 	case "/personal/insta/":
-		goTo(base + "/insta")
+		goTo(base + "/insta");
 		break;
 	case "/personal/insta/token":
 	case "/personal/insta/token/":
-		goTo(base + "/insta/token")
+		goTo(base + "/insta/token");
 		break;
 	case "/redirect":
 	case "/redirect/":
-		goTo(window.location.search.replace("?rdr=", ""))
+		goTo(window.location.search.replace("?rdr=", ""));
 		break;
 	
 	/* Others */
 	case "/314":
-		goTo(base + "/personal/txts/pi_nospace.txt")
+		goTo(base + "/personal/txts/pi_nospace.txt");
 		break;
 	case "/home":
-		goTo(base)
+		goTo(base);
 		break;
 	case "/src":
 	case "/source":
-		goTo("https://github.com/alir6716/alir6716.github.io")
+		goTo("https://github.com/alir6716/alir6716.github.io");
 		break;
 	case "/ig":
 	case "/ig/":
-		goTo(base + "/insta")
+		goTo(base + "/insta");
 		break;
 	case "/igtoken":
 	case "/igtoken/":
-		goTo(base + "/insta/token")
+		goTo(base + "/insta/token");
 		break;
 	
 	/* Random */
 	case "/blondielaugh":
 	case "/gilgameshlaugh":
-		goTo("https://www.youtube.com/watch?v=1VD70_8IN1w")
+		goTo("https://www.youtube.com/watch?v=1VD70_8IN1w");
 		break;
 	case "/excalibur":
-		goTo("https://www.artstation.com/embed/1765306")
+		goTo("https://www.artstation.com/embed/1765306");
 		break;
 	case "/FateZero":
-		goTo(base + "/personal/zips/FateZero.zip")
+		goTo(base + "/personal/zips/FateZero.zip");
 		break;
 	
 	/* Default */
