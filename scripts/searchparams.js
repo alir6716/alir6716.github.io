3e94fb518cab0a8889d1f39868c3dcbf922c9043
alir6716 <alir6716@gmail.com>
2016-04-28 17:38:58 +0100

    /insta_old removed and /scripts/searchparams.js added.

---
+++ scripts/searchparams.js
@@ -0,0 +1,8 @@
+// Get URL/Hash Parameters.
+// Created by AR.
+function getURLParameter(name) {
+	return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||undefined;
+};
+function getHashParameter(name) {
+	return decodeURIComponent((new RegExp('[#|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.hash)||[,""])[1].replace(/\+/g, '%20'))||undefined;
+};
